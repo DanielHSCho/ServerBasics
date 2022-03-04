41dd16dd12e25e47ec819924de45601a23d0f715
daniel <hellena171791@gmail.com>
2022-03-04 15:57:59 +0900

    <패킷 제네레이터 6#> 패킷 매니저 자동화 후처리 (PacketHandler에 TestHandler 추가)

--- ServerCore/Server/Packet/PacketHandler.cs
+++ ServerCore/Server/Packet/PacketHandler.cs
@@ -3,20 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Server
+class PacketHandler
 {
-    class PacketHandler
+    public static void PlayerInfoReqHandler(PacketSession session, IPacket packet)
     {
-        // 1. 함수 이름은 최대한 패킷이름과 동일하게 맞추자 (PlayerInfoReq -> PlayerInfoReqHandler)
-        public static void PlayerInfoReqHandler(PacketSession session, IPacket packet)
-        {
-            // 2. 캐스팅
-            PlayerInfoReq req = packet as PlayerInfoReq;
-            Console.WriteLine($"PlayerInfoReq:{req.playerId} {req.name}");
+        PlayerInfoReq req = packet as PlayerInfoReq;
+        Console.WriteLine($"PlayerInfoReq:{req.playerId} {req.name}");
 
-            foreach (PlayerInfoReq.Skill skill in req.skills) {
-                Console.WriteLine($"Skill({skill.id}:{skill.level}:{skill.duration})");
-            }
+        foreach (PlayerInfoReq.Skill skill in req.skills) {
+            Console.WriteLine($"Skill({skill.id}:{skill.level}:{skill.duration})");
         }
     }
+
+    public static void TestHandler(PacketSession session, IPacket packet)
+    {
+
+    }
 }
+
+
+
+// <패킷 제네레이터 5#> 22.03.04 - 패킷 핸들러 추가
+//namespace Server
+//{
+//    class PacketHandler
+//    {
+//        // 1. 함수 이름은 최대한 패킷이름과 동일하게 맞추자 (PlayerInfoReq -> PlayerInfoReqHandler)
+//        public static void PlayerInfoReqHandler(PacketSession session, IPacket packet)
+//        {
+//            // 2. 캐스팅
+//            PlayerInfoReq req = packet as PlayerInfoReq;
+//            Console.WriteLine($"PlayerInfoReq:{req.playerId} {req.name}");
+
+//            foreach (PlayerInfoReq.Skill skill in req.skills) {
+//                Console.WriteLine($"Skill({skill.id}:{skill.level}:{skill.duration})");
+//            }
+//        }
+//    }
+//}
