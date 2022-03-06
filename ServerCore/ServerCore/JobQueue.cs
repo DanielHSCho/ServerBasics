c9d7bc44ff07420980bc86cbc8574452ce360f61
daniel <hellena171791@gmail.com>
2022-03-06 15:59:00 +0900

    <JobQueue 1#> JobQueue 추가
    - ServerCore : JobQueue 추가
    - IJobQueue 인터페이스 추가

---
+++ ServerCore/ServerCore/JobQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerCore
+{
+    public interface IJobQueue
+    {
+        void Push(Action job);
+    }
+
+    class JobQueue : IJobQueue
+    {
+        // 내가 해야하는 일감들을 가지고 있는 큐
+        Queue<Action> _jobQueue = new Queue<Action>();
+        object _lock = new object();
+
+        public void Push(Action job)
+        {
+            lock (_lock) {
+                _jobQueue.Enqueue(job);
+            }
+        }
+
+        private Action Pop()
+        {
+            lock (_lock) {
+                if(_jobQueue.Count == 0) {
+                    return null;
+                }
+                return _jobQueue.Dequeue();
+            }
+        }
+    }
+}
